3b4278eeb40be12e1cd2bab22950bde97d4c0651
Kushal Dabbe <72650064+kushal752@users.noreply.github.com>
2021-06-05 20:08:28 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,5 @@
 import React from 'react';
 
-//This component will be a functional component
-//create div with a class of nav-wrapper(this is some kind of shortcut)
-//These classes are coming from the materialize CSS framework
-//Now go ahead and add a link there and give it a className brandlogo
-//We need to include the component into App.js and also export it
-//Include it inside the div in App.js
-//Our Navbar is completed!!
-//Then create the SearchArea.js inside the components folder
-
-
 const Nav = () => {
    return (
      <nav>
